680e63568e82d9d1b85070c4301c6c42218134b1
Bouchahwa Abdelwaheb <abdelwahebbouchahwa52@gmail.com>
2025-05-04 22:10:19 +0100

    Fixing the size conflict

--- src/main/java/com/pharmacy/app/App.java
+++ src/main/java/com/pharmacy/app/App.java
@@ -11,10 +11,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Dashboard.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/PrimaryStage.fxml"));
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("Login Now !");
-        //primaryStage.setResizable(false);
+        primaryStage.setResizable(false);
         //primaryStage.setFullScreen(true);
         primaryStage.show();
     }
