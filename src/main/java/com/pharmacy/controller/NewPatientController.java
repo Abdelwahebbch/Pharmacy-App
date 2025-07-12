ea23fe03b8b7cba689921149cf22e3a6c534eb91
Bouchahwa Abdelwaheb <abdelwahebbouchahwa52@gmail.com>
2025-07-12 11:50:46 +0100

    implémentaion des tous les focntionalités de "NewPatientController"

--- src/main/java/com/pharmacy/controller/NewPatientController.java
+++ src/main/java/com/pharmacy/controller/NewPatientController.java
@@ -1,12 +1,17 @@
 package com.pharmacy.controller;
 
+import com.pharmacy.DAO.PatientsDAO;
+import com.pharmacy.Model.Patient;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class NewPatientController {
 
@@ -14,45 +19,51 @@ public class NewPatientController {
     private TextField PhoneField;
 
     @FXML
-    private Button addButton;
-
-    @FXML
-    private Button clearButton;
-
-    @FXML
-    private Button deleteButton;
-
-    @FXML
     private TextField doctorNameField;
 
     @FXML
-    private DatePicker expiryDatePicker;
+    private DatePicker birthdayDatePicker;
 
     @FXML
-    private DatePicker issueDatePicker;
-
-    @FXML
-    private TextArea medicationsArea;
+    private TextArea notesArea;
 
     @FXML
     private TextField patientNameField;
 
     @FXML
-    private ComboBox<?> statusComboBox;
+    void handleAddPatientButton(ActionEvent event) {
+        Patient p = buildPatientFromForm();
+        PatientsDAO.createPatient(p);
+        showInfo("Sccess !", "The new patient named \" " + p.getName() + " \" was created successfully");
+    }
 
-    @FXML
-    void handleAddPrescription(ActionEvent event) {
+    private Patient buildPatientFromForm() {
 
+        return new Patient(patientNameField.getText(), PhoneField.getText(), doctorNameField.getText(),
+                java.sql.Date.valueOf(birthdayDatePicker.getValue()), notesArea.getText());
     }
 
     @FXML
     void handleClearFields(ActionEvent event) {
-
+        patientNameField.clear();
+        doctorNameField.clear();
+        PhoneField.clear();
+        notesArea.clear();
+        birthdayDatePicker.setValue(null);
     }
 
     @FXML
-    void handleDeletePrescription(ActionEvent event) {
+    void handleCancelButton(ActionEvent event) {
+        Stage s = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        s.close();
+    }
 
+    private void showInfo(String title, String message) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
 }
