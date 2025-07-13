30f394c58b1b1ca42c8dc1572f9efb2c5225e1e2
Bouchahwa Abdelwaheb <abdelwahebbouchahwa52@gmail.com>
2025-07-13 10:24:44 +0100

    Fixing moste of fun in patient Scene : - searche with (name/birthday/phone) - Define the table columns

--- src/main/java/com/pharmacy/controller/PatientController.java
+++ src/main/java/com/pharmacy/controller/PatientController.java
@@ -1,16 +1,26 @@
 package com.pharmacy.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.pharmacy.DAO.PatientsDAO;
+import com.pharmacy.Model.Patient;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
-public class PatientController {
+public class PatientController implements Initializable {
 
     @FXML
     private TextField PhoneField;
@@ -46,7 +56,7 @@ public class PatientController {
     private TextField patientNameField;
 
     @FXML
-    private TableView<?> patientsTable;
+    private TableView<Patient> patientsTable;
 
     @FXML
     private TableColumn<?, ?> phoneColumn;
@@ -56,6 +66,8 @@ public class PatientController {
 
     @FXML
     private Button updateButton;
+
+    private ObservableList<Patient> patientList = FXCollections.observableArrayList();
 
     @FXML
     void handleAddPatient(ActionEvent event) {
@@ -79,7 +91,19 @@ public class PatientController {
 
     @FXML
     void handleSearchPatient(ActionEvent event) {
-
+        String p = searchField.getText();
+        if (p.isEmpty()) {
+            patientsTable.setItems(patientList);
+            return;
+        }
+        ObservableList<Patient> filtredList = FXCollections.observableArrayList();
+        for (Patient i : patientList) {
+            if (i.getName().toLowerCase().contains(p) || i.getPhone().toLowerCase().contains(p)
+                    || String.valueOf(i.getBirthday()).contains(p)) {
+                filtredList.add(i);
+            }
+        }
+        patientsTable.setItems(filtredList);
     }
 
     @FXML
@@ -87,4 +111,24 @@ public class PatientController {
 
     }
 
+    private void initializeColumns() {
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        patientNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        birthdayColumn.setCellValueFactory(new PropertyValueFactory<>("birthday"));
+        doctorNameColumn.setCellValueFactory(new PropertyValueFactory<>("familyDoctor"));
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        initializeColumns();
+        PatientsDAO.LoadAllPatients(patientList);
+        patientsTable.setItems(patientList);
+
+    }
+
+    private Patient buildPatientFromForm() {
+
+        return new Patient(patientNameField.getText(), PhoneField.getText(), doctorNameField.getText(),
+                java.sql.Date.valueOf(birthdayDatePicker.getValue()), notesArea.getText());
+    }
 }
